db5813af698d32549b646c339c67fa145e7b6156
Marco Zwyssig <marco@zwyssig.info>
2021-01-06 12:39:13 +0100

    new version

---
+++ beta/update.cmd
@@ -0,0 +1,4 @@
+XCOPY c:\git\marcozwyssig\cleon\src\cleonproject.deliverables.architecture.model.architecture\src\cleon.site\features\* .\features /s /i /y
+XCOPY c:\git\marcozwyssig\cleon\src\cleonproject.deliverables.architecture.model.architecture\src\cleon.site\plugins\* .\plugins /s /i /y
+xcopy c:\git\marcozwyssig\cleon\src\cleonproject.deliverables.architecture.model.architecture\src\cleon.site\content.jar .\ /s /i /y
+xcopy c:\git\marcozwyssig\cleon\src\cleonproject.deliverables.architecture.model.architecture\src\cleon.site\artifacts.jar .\ /s /i /y
