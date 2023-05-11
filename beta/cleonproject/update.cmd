b83b21f296ef5d6bea5f409f326afafe0457c59e
Marco Zwyssig <marco@zwyssig.info>
2023-05-11 14:10:40 +0200

    new version

---
+++ beta/cleonproject/update.cmd
@@ -0,0 +1,4 @@
+XCOPY c:\git\marcozwyssig\cleon\src\cleonproject.site\features\* .\features /s /i /y
+XCOPY c:\git\marcozwyssig\cleon\src\cleonproject.site\plugins\* .\plugins /s /i /y
+xcopy c:\git\marcozwyssig\cleon\src\cleonproject.site\content.jar .\ /s /i /y
+xcopy c:\git\marcozwyssig\cleon\src\cleonproject.site\artifacts.jar .\ /s /i /y
